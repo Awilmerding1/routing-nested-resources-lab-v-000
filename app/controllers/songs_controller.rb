ecfb80d2880b2fedbb34fcbe5c19979a4c324749
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-01 18:58:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   
   def index
-    if params[:artist_id]
+    if params[:artist_id] && Artist.find_by(id: params[:artist_id]) != nil
       @songs = Artist.find_by(id: params[:artist_id]).songs
     else 
       @songs = Song.all
